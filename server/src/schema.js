21bc35379062d7dd336ca7f130b69c00a42a68a8
Hector Diaz Diaz <e-hdiaz@derco.cl>
2023-02-09 17:32:31 -0300

    add detail page using graphql arguments

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,12 +1,14 @@
-const { gql } = require('apollo-server');
+const { gql } = require("apollo-server");
 
 const typeDefs = gql`
-type Query {
+  type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-}
-"A track is a group of Modules that teaches about a specific topic"
-type Track {
+    "Fetch a specific track, provided a track's ID"
+    track(id: ID!): Track
+  }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
     id: ID!
     "the track's title"
     title: String!
@@ -18,16 +20,31 @@ type Track {
     length: Int
     "the number of modules this track contains"
     modulesCount: Int
-}
+    "The track's complete description, can be in Markdown format"
+    description: String
+    "The number of times a track has been viewed"
+    numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
+  }
 
-"Author of a complete track"
-type Author {
+  "A Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
+  }
+
+  "Author of a complete track"
+  type Author {
     id: ID!
     "Author's first and last name"
     name: String!
     "Author's profile picture url"
     photo: String
-}
+  }
 `;
 
 module.exports = typeDefs;
